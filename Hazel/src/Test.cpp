caf47a0fe98e1c3ffa0a5125eb90aa556e76e175
BL-sy <3026709446@qq.com>
2025-10-15 14:40:36 +0800

    入口点

--- Hazel/src/Test.cpp
+++
@@ -1,8 +0,0 @@
-#include "Test.h"
-
-namespace Hazel {
-	void Print()
-	{
-		printf("test");
-	}
-}
--- Hazel/src/Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-namespace Hazel {
-	__declspec(dllexport) void Print();
-}
